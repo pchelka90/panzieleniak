d58a67d127c67686be2265cb7adcce32cd476954
Bartek Bronkowski <bartlomiejbronkowski90@gmail.com>
2024-05-11 18:16:09 +0200

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -137,7 +137,7 @@
 		
 			
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-			<link rel="stylesheet" href="style.css" type="text/css">
+			<link rel="stylesheet" href="css/style.css" type="text/css">
 			<link rel="stylesheet" href="css/fontello.css" type="text/css">
                         <link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	
