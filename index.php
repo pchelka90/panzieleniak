31cd0e7e7c6b6c152b694e797e58c158cc085faa
Bartek Bronkowski <bartlomiejbronkowski90@gmail.com>
2024-05-11 18:11:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -137,20 +137,9 @@
   }
 ?>
 	<div class="footer">
-		tworz_stopke_html();
-				
-		<div class="lefty">
-			<a href="panzieleniak.php">panzieleniak.pl</a>
-			<br />
-			<a href="mailto:kontakt@panzieleniak.pl">kontakt@panzieleniak.pl</a>
-			<br />
-			+48 601 150 149
-		</div>
-		<div class="right">panzieleniak&copy;wszelkie prawa zastrzeżone
-			<br />
-			kreacja: <a href="https://pchelka90.github.io/portfolio-website/" target="_blank">Bartłomiej Bronkowski - portfolio</a>
-		</div>
-				
+		<?php
+			tworz_stopke_html();
+		?>	
 	</div>
 
   </div>
