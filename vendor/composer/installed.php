a499dc2ebee26918ffcf80c073d8779b1db3e33a
Bartek B. <bartlomiejbronkowski90@gmail.com>
2024-05-12 23:42:25 +0200

    Upload composer.lock file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pchelka90/panzieleniak',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '90904904e6ed0c217a15ea497e82b649c2805d65',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pchelka90/panzieleniak' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '90904904e6ed0c217a15ea497e82b649c2805d65',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
