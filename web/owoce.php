63bd7a50699045ac4709b8113c808da396c015fc
Bartek Bronkowski <bartlomiejbronkowski90@gmail.com>
2025-05-29 14:29:33 +0200

    Update owoce.php

--- web/owoce.php
+++ web/owoce.php
@@ -17,6 +17,11 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 		<link rel="stylesheet" href="stylesheet/css/fontello.css" type="text/css">
 		<link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<link rel="preload" href="stylesheet/font/lato-italic-webfont.woff" as="font" type="font/woff" crossorigin>
+		<link rel="preload" href="stylesheet/font/lato-italic-webfont.woff2" as="font" type="font/woff2" crossorigin>
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link rel="apple-touch-icon" href="/favicon.ico">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 	</head>
 
 <body>
