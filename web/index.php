97e16fb5f7b5c8ec9deb1c3e84622558cb78c3a3
Bartek Bronkowski <bartlomiejbronkowski90@gmail.com>
2025-06-04 20:13:40 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -106,7 +106,7 @@
 				<a href="index.php"><span class="pan">pan</span><span class="zieleniak">zieleniak.pl</span></a>
 			</div>
 			
-			<div class="categorydescriptioncontent">
+			<div class="categorydescriptioncontent menu">
 				<button class="action close" aria-label="Left Align">
 					<span class="material-icons" aria-hidden="true"><i class="material-icons icon-list-bullet"></i></span>
 				</button>
